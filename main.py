583f35d9a60cf1f39d9594768782f20a7fccdd61
Peter Åslund <peter@peteraslund.me>
2025-01-15 09:58:22 +0100

    Initial implementation of OpenHardwareMonitor

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import asyncio
+import json
+from openhardwaremonitor import OpenHardwareMonitor
+
+async def main():
+    ohm = OpenHardwareMonitor('192.168.1.114', 8085)
+    data = await ohm.get_data()
+    json.dumps(data)
+    await ohm._api.close()
+
+if __name__ == '__main__':
+    asyncio.run(main())
