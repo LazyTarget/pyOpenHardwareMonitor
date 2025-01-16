7f6f5ac0d9363678a78d27d20772d170d59b767e
Peter Åslund <peter@peteraslund.me>
2025-01-16 07:41:52 +0100

    Corrected printing of resulting json

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import asyncio
 import json
 from openhardwaremonitor import OpenHardwareMonitor
 
+
 async def main():
     ohm = OpenHardwareMonitor('192.168.1.114', 8085)
     data = await ohm.get_data()
-    json.dumps(data)
+    print(json.dumps(data))
     await ohm._api.close()
 
 if __name__ == '__main__':
